8f59dd6da70e307d47606d9165ca296dfe22447c
anjali-dange-06 <coderg75@gmail.com>
2026-08-13 12:31:53 +0530

    Add files via upload

---
+++ File_Hadling.py
@@ -0,0 +1,118 @@
+# FILE HANDLING IN PYTHON
+
+# CREATE FILE
+def create_file():
+    filename = input("Enter file name: ")
+
+    try:
+        file = open(filename, "x")
+        file.close()
+        print("File created successfully")
+
+    except FileExistsError:
+        print("File already exists")
+
+
+# READ FILE
+def read_file():
+    filename = input("Enter file name: ")
+
+    try:
+        file = open(filename, "r")
+
+        data = file.read()
+        print("\nFile content:")
+        print(data)
+
+        file.close()
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+# WRITE FILE
+def write_file():
+    filename = input("Enter file name: ")
+
+    try:
+        file = open(filename, "w")
+
+        data = input("Enter data to write: ")
+        file.write(data)
+
+        file.close()
+
+        print("Data written successfully")
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+# APPEND FILE
+def append_file():
+    filename = input("Enter file name: ")
+
+    try:
+        file = open(filename, "a")
+
+        data = input("Enter data to append: ")
+        file.write("\n" + data)
+
+        file.close()
+
+        print("Data appended successfully")
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+# DELETE FILE
+def delete_file():
+    filename = input("Enter file name: ")
+
+    try:
+        import os
+
+        os.remove(filename)
+
+        print("File deleted successfully")
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+# MAIN PROGRAM
+
+while True:
+
+    print("\n===== FILE OPERATIONS =====")
+    print("1. Create File")
+    print("2. Read File")
+    print("3. Write File")
+    print("4. Append File")
+    print("5. Delete File")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        create_file()
+
+    elif choice == "2":
+        read_file()
+
+    elif choice == "3":
+        write_file()
+
+    elif choice == "4":
+        append_file()
+
+    elif choice == "5":
+        delete_file()
+
+    elif choice == "6":
+        print("Program ended")
+        break
+
+    else:
+        print("Invalid choice")
